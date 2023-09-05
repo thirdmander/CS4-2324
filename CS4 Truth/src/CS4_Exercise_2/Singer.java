a71b058cf2b23205199eb962a4376ab0c73a29b0
Third <laureano3rd23@gmail.com>
2023-09-05 13:27:39 +0800

    modify files

--- CS4 Truth/src/CS4_Exercise_2/Singer.java
+++ CS4 Truth/src/CS4_Exercise_2/Singer.java
@@ -1,26 +1,26 @@
 package CS4_Exercise_2;
 
 public class Singer{
-    String name;
+    String artistName;
     int noOfPerformances;
     double earnings;
     Song favoriteSong;
     
     public void performForAudience(int noOfPeople){
-    //increases noOfPerformances by 1 and earnings by 100 for each person (e.g. 5 people results in 500 increase)
+    //increases noOfPerformances by 1 and earnings by 100 for each person
         noOfPerformances += 1;
         earnings += 100 * noOfPeople;
     }
     public void changeFavSong(Song newSong){
     //changes favoriteSong
-        favoriteSong = newSong;
+        this.favoriteSong = newSong;
     }
 
-    public Singer(String artistName, int performancesCount, double totalEarnings, Song songFavorite){
-        name = artistName;
-        noOfPerformances = performancesCount;
-        earnings = totalEarnings;
-        favoriteSong = songFavorite;
+    public Singer(String artistName, int noOfPerformances, double earnings, Song favoriteSong){
+        this.artistName = artistName;
+        this.noOfPerformances = noOfPerformances;
+        this.earnings = earnings;
+        this.favoriteSong = favoriteSong;
     }
 }
 
