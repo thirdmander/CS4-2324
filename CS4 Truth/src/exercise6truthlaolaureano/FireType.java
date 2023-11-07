69d29266df1282b1d303a6b6d9ddfca573e7bd39
Third <laureano3rd23@gmail.com>
2023-11-07 14:11:53 +0800

    exercise 6 new branch

---
+++ CS4 Truth/src/exercise6truthlaolaureano/FireType.java
@@ -0,0 +1,11 @@
+package exercise6truthlaolaureano;
+
+public class FireType {
+    private String name;
+    
+    public FireType(String name, int hp, int base){
+        super();
+        this.name = name;
+        this.   
+    }
+}
