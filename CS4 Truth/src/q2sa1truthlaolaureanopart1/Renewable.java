c45748cb0c0b90ac866fa8a68098d21e9dc185d4
Third <laureano3rd23@gmail.com>
2024-02-09 20:34:39 +0800

    finished exercise 11

--- CS4 Truth/src/q2sa1truthlaolaureanopart1/Renewable.java
+++
@@ -1,5 +0,0 @@
-package q2sa1truthlaolaureanopart1;
-
-public interface Renewable {
-    public abstract void renew();
-}
--- CS4 Truth/src/q2sa1truthlaolaureanopart2/InternetCapable.java
+++
@@ -1,5 +0,0 @@
-package q2sa1truthlaolaureanopart2;
-
-public interface InternetCapable{
-    public abstract void connect(String s);
-}
