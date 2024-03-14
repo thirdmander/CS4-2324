c8af70c349b60ef862cba0140e9217a2ed2c1907
Third <laureano3rd23@gmail.com>
2024-03-14 21:15:46 +0800

    exercise 14 finished

--- CS4 Truth/lib/Truth-Lao-3rdQtrMilestone1/App.java
+++ CS4 Truth/lib/Truth-Lao-3rdQtrMilestone1/App.java
@@ -7,10 +7,11 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        //title screen has buttons that open other screens however the other screens aren't finished yet
+        Parent root = FXMLLoader.load(getClass().getResource("TitleScreen.fxml"));
         Scene scene = new Scene(root);
       
-        primaryStage.setTitle("Exercise 13");
+        primaryStage.setTitle("Milestone 1");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
